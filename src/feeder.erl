b3ef60aee5ea400433cd1cd61b069ff6c292736a
Michael Nisi <michael.nisi@gmail.com>
2013-11-10 17:30:31 +0100

    Parse entry title

--- src/feeder.erl
+++ src/feeder.erl
@@ -4,35 +4,93 @@
 -module(feeder).
 -export([stream/2]).
 
+%% API
+
+%% Events
+%% {feed, Feed}, State
+%% {entry, Entry}, State
+%% endFeed, State
 stream(Chunk, UserOpts) ->
   Opts = opts(UserOpts),
   xmerl_sax_parser:stream(Chunk, Opts).
 
+%% Tuples
+
 opts(Opts) -> 
   ContinuationState = proplists:get_value(continuation_state, Opts),
   ContinuationFun = proplists:get_value(continuation_fun, Opts),
+  UserState = proplists:get_value(event_state, Opts),
+  UserFun = proplists:get_value(event_fun, Opts),
+  User = {UserState, UserFun},
   [{continuation_state, ContinuationState}
  , {continuation_fun, ContinuationFun}
- , {event_state, state()}
+ , {event_state, state(User)}
  , {event_fun, fun event/3}].
 
-state() ->
-  {}.
+-define(STATE, {_Chars, _Feed, _Entry, _User}).
+state(User) ->
+  {nil, nil, nil, User}.
 
-qname({_, Name}) ->
-  list_to_atom(Name).
+-define(IS_FEED, _Feed =/= null, _Entry =:= nil).
+-define(FEED, {_Title, _Summary, _Link}).
+feed() ->
+  {nil, nil, nil}.
 
-start_element(E, _S) ->
-  io:format("start ~p~n", [E]).
+-define(IS_ENTRY, _Entry =/= nil).
+-define(ENTRY, {_Title, _Summary, _Link, _PubDate}).
+entry() ->
+  {nil, nil, nil, nil}.
 
-end_element(E, _S) ->
-  io:format("end ~p~n", [E]).
+entry(title, Title, ?ENTRY) ->
+  {Title, _Summary, _Link, _PubDate};
+entry(summary, Summary, ?ENTRY) ->
+  {_Title, Summary, _Link, _PubDate}.
 
+chars() ->
+  [].
+
+%% Event handlers
+
+start_element(item, ?STATE) ->
+  {_Chars, _Feed, entry(), _User};
+start_element(title, ?STATE) ->
+  {chars(), _Feed, _Entry, _User};
+start_element(_, S) ->
+  S.
+
+end_element(title, ?STATE) when ?IS_ENTRY ->
+  Title = chars(_Chars),
+  Entry = entry(title, Title, _Entry),
+  {nil, _Feed, Entry, _User}; 
+end_element(item, ?STATE) ->
+  {UserState, UserFun} = _User,
+  UserFun({entry, _Entry}, UserState),
+  {_Chars, _Feed, nil, _User};
+end_element(_, S) ->
+  S.
+
+%% SAX events
+
+event(startDocument, _, S) ->
+  S;
 event({startElement, _, _LocalName, QName, _Attrs}, _, S) ->
   start_element(qname(QName), S);
 event({endElement, _, _LocalName, QName}, _, S) ->
   end_element(qname(QName), S);
-event({characters, C}, _, S) ->
+event({characters, C}, _, ?STATE) ->
+  %% TODO: Use binaries instead of lists
+  {[_Chars|C], _Feed, _Entry, _User};
+event(endDocument, _, S) ->
+  {_, _, _, {UserState, UserFun}} = S,
+  UserFun(endFeed, UserState),
   S;
 event(_, _, S) ->
   S.
+
+%% Internal funs
+
+qname({_, Name}) ->
+  list_to_atom(Name).
+
+chars(C) ->
+  list_to_binary(C).
--- test/feeder_test.erl
+++ test/feeder_test.erl
@@ -3,7 +3,26 @@
 
 -include_lib("etest/include/etest.hrl").
 
-test_stream() ->
-  Expected = {ok, ok, []},
-  Actual = feeder:stream("<thing></thing>", []),
-  ?assert_equal(Expected, Actual). 
+test_item() ->
+  Opts = [{event_state, {}}, {event_fun, fun event/2}], 
+  feeder:stream("<channel><item><title>Nice Title</title></item><item><title>Nice Title</title></item></channel>", Opts),
+  Entries = receive_entries([]),
+  [?assert_equal(<<"Nice Title">>, Title) || {Title, _, _, _} <- Entries],
+  ?assert_equal(2, length(Entries)).
+
+receive_entries(Entries) ->
+  receive 
+    {entry, Entry} ->
+      receive_entries([Entry|Entries]);
+    endFeed ->
+      Entries
+  end.
+
+event({entry, Entry}, S) ->
+  self() ! {entry, Entry},
+  S;
+event(endFeed, S) ->
+  self() ! endFeed,
+  S;
+event(_, S) ->
+  S.
