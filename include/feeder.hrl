8cee566d4b524e9ddd9e9c5a33dffb0f62f02a90
Michael Nisi <michael.nisi@gmail.com>
2013-12-14 18:05:40 +0100

    Remove unused fields

--- include/feeder.hrl
+++ include/feeder.hrl
@@ -1,23 +1,18 @@
 
 -record(feed, {
     author,
-    copyright,
     id,
     image,
-    language,
     link,
-    payment,
     subtitle,
     summary,
     title,
-    ttl,
     updated
   }).
 
 -record(entry, {
     author,
     enclosure,
-    duration,
     id,
     image,
     link,
