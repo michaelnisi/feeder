f81a665be6973f02e9dd59ecc14f08656838a64a
Michael Nisi <michael.nisi@gmail.com>
2016-07-30 12:03:41 +0200

    Bump version

--- package.exs
+++ package.exs
@@ -3,7 +3,7 @@ defmodule Feeder.Mixfile do
 
   def project do
     [app: :feeder,
-      version: "2.0.0",
+      version: "2.0.1",
       description: description,
       package: package,
       deps: deps]
