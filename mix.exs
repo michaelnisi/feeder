cb9aeda4b3eb5dcc17dea562c1577ba5570a2b08
Michael Nisi <michael.nisi@gmail.com>
2019-06-14 07:44:34 +0200

    Correct version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Feeder.Mixfile do
 
   def project do
     [app: :feeder,
-     version: "2.3.1",
+     version: "2.3.2",
      description: description(),
      package: package(),
      deps: deps(),
